ff8f5136bc37a3bbc267838e27ea30b93de6aa2c
tenten0213 <takehito.0213@gmail.com>
2015-09-22 23:14:13 +0900

    add service methods

--- java-jax-rs/todo/src/main/java/tenten0213/todo/TodoService.java
+++ java-jax-rs/todo/src/main/java/tenten0213/todo/TodoService.java
@@ -1,9 +1,34 @@
 package tenten0213.todo;
 
+import tenten0213.entity.TodoEntity;
+
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @RequestScoped
 @Transactional
 public class TodoService {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public TodoEntity save(TodoEntity entity) {
+        em.persist(entity);
+        return entity;
+    }
+
+    public List<TodoEntity> findAll() {
+        TypedQuery<TodoEntity> query = em.createNamedQuery("todo_findAll", TodoEntity.class);
+        query.setMaxResults(10);
+        return query.getResultList();
+    }
+
+    public void delete(Long todoId) {
+        TodoEntity entity = em.find(TodoEntity.class, todoId);
+        em.remove(entity);
+    }
 }
